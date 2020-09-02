69a79a02061c1bbe0afc5ab6c700f436d74a5e02
Kurimbah <Kurimbah@gmail.com>
2020-09-02 04:47:58 -0500

    Commit the change

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace VisualStudio2019CSharpGitDemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Here is a change.");
         }
     }
 }
